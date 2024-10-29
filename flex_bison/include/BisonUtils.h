62a64ba8b3c8ad79edd52c8e85909a1e32cc272c
bardoor <dmitrybartenev58@gmail.com>
2024-10-30 02:46:54 +0300

    fix(BisonUtils): исправил ошибку с инициализатором ссылочных параметров

--- flex_bison/include/BisonUtils.h
+++ flex_bison/include/BisonUtils.h
@@ -8,63 +8,63 @@
 
 struct Node {
 public:
-    Node() : id(nextId++) { id = nextId; }
+    Node() : id(nextId++) {} // убрано дублирующее присвоение id
     virtual std::string generateDot() = 0;
     virtual ~Node() = default;
-    
+
 protected:
     static int nextId;
     long long id;
 };
 
-struct Param : public Node {
+int Node::nextId = 0; // инициализация статического члена nextId
 
+struct Param : public Node {
+public:
+    Param(std::string& paramName) : name(paramName) {} // инициализация ссылки name
+
+protected:
+    std::string& name;
 };
 
 struct ParamList : public Node {
 public:
-    ParamList(std::vector<Param*>& parameters) {
-        params = parameters;
-    }
+    ParamList(std::vector<Param*>& parameters) : params(parameters) {} // инициализация params
 
 protected:
     std::vector<Param*> params;
 };
 
-
-struct FuncDecl {
+struct FuncDecl : public Node {
 public:
-    FuncDecl(std::string& functionName, ParamList* parametersList) {
-        name = functionName;
-        paramList = std::unique_ptr<ParamList>(parametersList);
-    }
+    FuncDecl(std::string& functionName, ParamList* parametersList, Expr* funcBody)
+        : name(functionName), paramList(parametersList), body(funcBody) {} // инициализация всех полей
 
 protected:
     std::string& name;
     std::unique_ptr<ParamList> paramList;
+    std::unique_ptr<Expr> body;
 };
 
 struct Module {
-    Module(Expression* e) {
-        expr = e;
-    }
-    
-    Expression* expr;
+public:
+    Module(FuncDecl* fDecl) : decl(fDecl) {} // инициализация decl
+
+protected:
+    FuncDecl* decl;
 };
 
-struct Expression {
-    virtual ~Expression() = default;
+struct Expr {
+    virtual ~Expr() = default;
 };
 
-struct BinaryExpr : public Expression {
-    BinaryExpr(Expression* l, Expression* r) : left(l), right(r) {}
-    Expression* left;
-    Expression* right;
+struct BinaryExpr : public Expr {
+    BinaryExpr(Expr* l, Expr* r) : left(l), right(r) {} // инициализация left и right
+    Expr* left;
+    Expr* right;
 };
 
-struct NumericLiteral : public Expression {
-    NumericLiteral(long long v) : val(v) {}
+struct IntLiteral : public Expr {
+    IntLiteral(long long v) : val(v) {} // инициализация val
     long long val;
 };
-
-
