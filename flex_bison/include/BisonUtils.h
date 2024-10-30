f0e6007e3019fde17ebc0ffb733a1dc3784e972d
bardoor <dmitrybartenev58@gmail.com>
2024-10-30 03:03:50 +0300

    fix(parser): убрал ошибку с ссылками в конструкторах узлов АСТ

--- flex_bison/include/BisonUtils.h
+++ flex_bison/include/BisonUtils.h
@@ -8,28 +8,32 @@
 
 struct Node {
 public:
-    Node() : id(nextId++) {} // убрано дублирующее присвоение id
-    virtual std::string generateDot() = 0;
+    Node() : id(nextId++) {}
+    //virtual std::string generateDot() = 0;
     virtual ~Node() = default;
 
+    static long long nextId;
+
 protected:
-    static int nextId;
     long long id;
 };
 
-int Node::nextId = 0; // инициализация статического члена nextId
-
 struct Param : public Node {
 public:
-    Param(std::string& paramName) : name(paramName) {} // инициализация ссылки name
+    Param(std::string paramName) : name(paramName) {} 
 
 protected:
-    std::string& name;
+    std::string name;
 };
 
 struct ParamList : public Node {
 public:
-    ParamList(std::vector<Param*>& parameters) : params(parameters) {} // инициализация params
+    ParamList() {} 
+    //ParamList(std::vector<Param*>& parameters) : params(parameters) {} 
+
+    void add(Param* param) {
+        params.push_back(param);
+    }
 
 protected:
     std::vector<Param*> params;
@@ -37,18 +41,17 @@ protected:
 
 struct FuncDecl : public Node {
 public:
-    FuncDecl(std::string& functionName, ParamList* parametersList, Expr* funcBody)
-        : name(functionName), paramList(parametersList), body(funcBody) {} // инициализация всех полей
-
+    FuncDecl(std::string functionName, ParamList* parametersList, Expr* funcBody)
+        : name(functionName), paramList(parametersList), body(funcBody) {} 
 protected:
-    std::string& name;
+    std::string name;
     std::unique_ptr<ParamList> paramList;
     std::unique_ptr<Expr> body;
 };
 
 struct Module {
 public:
-    Module(FuncDecl* fDecl) : decl(fDecl) {} // инициализация decl
+    Module(FuncDecl* fDecl) : decl(fDecl) {} 
 
 protected:
     FuncDecl* decl;
@@ -59,12 +62,12 @@ struct Expr {
 };
 
 struct BinaryExpr : public Expr {
-    BinaryExpr(Expr* l, Expr* r) : left(l), right(r) {} // инициализация left и right
+    BinaryExpr(Expr* l, Expr* r) : left(l), right(r) {} 
     Expr* left;
     Expr* right;
 };
 
 struct IntLiteral : public Expr {
-    IntLiteral(long long v) : val(v) {} // инициализация val
+    IntLiteral(long long v) : val(v) {} 
     long long val;
 };
