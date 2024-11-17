73a1961e430c77f3d59ed57a56f234cb8195fa7b
bardoor <dmitrybartenev58@gmail.com>
2024-11-18 01:09:05 +0300

    refactor(test): упростил запуск тестов

--- flex_bison/tests/test_all.py
+++ flex_bison/tests/test_all.py
@@ -2,14 +2,10 @@ import subprocess
 import pytest
 
 def run_parser(code: str, parser_name="bin/haskellc"):
-    return subprocess.run([parser_name, "-c", code], capture_output=True)
+    output = subprocess.run([parser_name, "-c", code], capture_output=True)
+    return (output.stdout.decode(), output.stderr.decode())
 
-def find_in_output(process: subprocess.CompletedProcess, substr: str):
-    return process.stdout.decode("utf-8").find(substr)
-     
 
 def test_constant_declare():
-    process = run_parser("a = x + 1")
-    assert find_in_output(process, "syntax error") == -1
-
-
+    (_, errors) = run_parser("a = x + 1")
+    assert "syntax error" not in errors
