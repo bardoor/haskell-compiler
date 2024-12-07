83b1c5bf3399d92722d1d85785a49aaa0f399cc1
bardoor <dmitrybartenev58@gmail.com>
2024-12-07 03:48:46 +0300

    feat(LayoutBuilder): добавил BeforeLexemState

--- flex_bison/include/LayoutBuild.hpp
+++ flex_bison/include/LayoutBuild.hpp
@@ -48,6 +48,42 @@ protected:
     std::unique_ptr<LayoutBuilder> owner;
 };
 
+
+
+/**
+ * Состояние в котором билдер находится в индетации перед первой лексемой в строке
+ * 
+ * Обязанности:
+ *  1. Прибавлять индентацию к текущему инденту LayoutBuilder в зависимости от типа токена
+ *  2. Переводить LayoutBuilder в состояние FirstLexemState при встрече с непробельным токеном
+ * 
+ */
+class BeforeLexemState : public LayoutBuilderState {
+public:
+    BeforeLexemState(LayoutBuilder* owner) : LayoutBuilderState(owner) {}
+
+    void addToken(std::vector<Token>::iterator& token) override {
+        switch (token->id){
+            case SPACE:
+                owner->addToIndent(1);
+                break;
+            case TAB:
+                owner->addToIndent(LayoutBuilder::TAB_SIZE);
+                break;
+            case NEWLINE:
+                owner->newLine();
+                break;
+            default:
+                // Не сдвигаем итератор, чтобы FirstLexemState получило текущий (непробельный) токен
+                owner->chageState(new FirstLexemState(owner.get()));
+                return;
+        }
+        // Сдвигаем итератор после пробельной лексемы
+        ++token;
+    }
+};
+
+
 LayoutBuilder::LayoutBuilder() 
     : state(std::make_unique<BeforeLexemState>(this)) {}
 
