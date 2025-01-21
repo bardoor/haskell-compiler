f1c9c51a1fc6022b558b4cd3637e44568b28c0c1
bardoor <dmitrybartenev58@gmail.com>
2025-01-21 11:07:30 +0300

    feat(gen_instr): добавил generate для операторов

--- semantic/lib/generators/gen_instr.ex
+++ semantic/lib/generators/gen_instr.ex
@@ -1,7 +1,12 @@
 defmodule Generators.GenInstr do
   alias Generators.Instruction, as: Instr
 
-  def generate(const_pool, %{cond: condition, then: then, else: else_expr}) do
+  def generate(const_pool, %{fun_decl: %{left: _left, right: right}}) do
+    # TODO: left пока игорируется, нужно будет на паттерн матчинге?
+    generate(const_pool, right)
+  end
+
+  def generate(const_pool, %{if: %{cond: condition, then: then, else: else_expr}}) do
     cond_instrs = generate(const_pool, condition)
     then_instrs = generate(const_pool, then)
     else_instrs = generate(const_pool, else_expr)
@@ -16,5 +21,32 @@ defmodule Generators.GenInstr do
       Instr.return()
     ])
   end
+
+  def generate(_const_pool, %{varid: varid}) do
+    Instr.load(varid)
+  end
+
+  def generate(const_pool, %{literal: %{type: type, value: value}}) do
+    Instr.load(const_pool, {type, Integer.parse(value)})
+  end
+
+  def generate(const_pool, %{op: %{type: type}, left: left, right: right}) do
+    type = String.to_atom(type)
+
+    op_instrs = case type do
+      :or -> Instr.ior()
+      :and -> Instr.iand()
+      _ -> Instr.icompare(type)
+    end
+
+    Instr.concat([
+      generate(const_pool, left),
+      generate(const_pool, right),
+      op_instrs
+    ])
+  end
+
+
+
 
 end
