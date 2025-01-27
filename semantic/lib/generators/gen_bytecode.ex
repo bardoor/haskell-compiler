0e0925df6f370ef281238c9f4d9854e6b4594d9b
bardoor <dmitrybartenev58@gmail.com>
2025-01-27 06:48:32 +0300

    feat(gen_bytecode): : добавил модуль генерации байткода

---
+++ semantic/lib/generators/gen_bytecode.ex
@@ -0,0 +1,149 @@
+defmodule Generators.GenBytecode do
+  import Bitwise
+  alias Generators.Instruction
+  alias Generators.GenMethod
+  alias Generators.ConstPool
+  alias Generators.GenClass
+
+  def generate(%GenClass{} = class) do
+    const_count = length(class.constant_pool) + 1
+    interfaces_count = length(class.interfaces)
+    fields_count = length(class.fields)
+    methods_count = length(class.methods)
+    this_class = ConstPool.constant_num(class.constant_pool, {:class, class.this_class})
+    super_class = ConstPool.constant_num(class.constant_pool, {:class, class.super_class})
+
+    <<0xCA, 0xFE, 0xBA, 0xBE>>
+    <> <<class.minor_version::16>>
+    <> <<class.major_version::16>>
+    <> <<const_count::16>>
+    <> bytify_const_pool(class.constant_pool)
+    <> bytify_class_access_flags(class.access_flags)
+    <> <<this_class::16>>
+    <> <<super_class::16>>
+    <> <<interfaces_count::16>>
+    <> bytify_interfaces(class.interfaces)
+    <> <<fields_count::16>>
+    <> bytify_fields(class.fields)
+    <> <<methods_count::16>>
+    <> bytify_methods(class.methods)
+  end
+
+
+  @spec bytify_const_pool(list()) :: binary()
+  defp bytify_const_pool(const_pool) do
+    Enum.reduce(const_pool, <<>>, fn const, acc ->
+      acc <> case const do
+        {:int, value}     -> <<0x03, value::32>>
+        {:class, num}     -> <<0x07, num::16>>
+        {:utf8, len, str} -> <<0x01, len::16, str>>
+        {:name_and_type, name, type}      -> <<0x12, name::16, type::16>>
+        {:class_method, class, name_type} -> <<0x10, class::16, name_type::16>>
+      end
+    end)
+  end
+
+  @spec bytify_class_access_flags(list()) :: binary()
+  defp bytify_class_access_flags(flags) do
+    mask = Enum.reduce(flags, 0, fn flag, acc ->
+      case flag do
+        :public -> acc ||| 0x0001
+        :final  -> acc ||| 0x0010
+        :super  -> acc ||| 0x0020
+        :enum   -> acc ||| 0x4000
+        :interface -> acc ||| 0x0200
+        :abstract  -> acc ||| 0x0400
+      end
+    end)
+
+    <<mask::16>>
+  end
+
+  @spec bytify_methods([%GenMethod{}]) :: binary()
+  defp bytify_methods(methods) do
+    Enum.reduce(methods, <<>>, fn method, acc ->
+      code_size = Instruction.size(method.code)
+
+      bytify_method_access_flags(method.access_flags)
+      <> <<method.name_num::16>>
+      <> <<method.descriptor_num::16>>
+      <> <<1::16>>
+      <> <<method.code_const_num::16>>
+      <> <<code_size + 12::16>>  # Длина атрибута
+      <> <<method.max_stack::16>>
+      <> <<method.max_locals::16>>
+      <> <<code_size::16>>
+      <> bytify_code(method.code)
+    end)
+  end
+
+  @spec bytify_code(%Instruction{}) :: binary()
+  defp bytify_code(code) do
+    Enum.reduce(code, <<>>, fn instr, acc -> acc <> bytify_instr(instr) end)
+  end
+
+  @spec bytify_instr([%Instruction{}]) :: binary()
+  defp bytify_instr(%Instruction{} = instr) do
+    case instr do
+      %Instruction{command: :iconst_m1} -> <<0x2>>
+      %Instruction{command: :iconst_0}  -> <<0x3>>
+      %Instruction{command: :iconst_1}  -> <<0x4>>
+      %Instruction{command: :iconst_2}  -> <<0x5>>
+      %Instruction{command: :iconst_3}  -> <<0x6>>
+      %Instruction{command: :iconst_4}  -> <<0x7>>
+      %Instruction{command: :iconst_5}  -> <<0x8>>
+
+      %Instruction{command: :bipush, arg: val} -> <<0x10, val>>
+      %Instruction{command: :sipush, arg: val} -> <<0x11, val::16>>
+
+      %Instruction{command: :if_icmpeq, arg: val} -> <<0x159, val::16>>
+      %Instruction{command: :if_icmpne, arg: val} -> <<0x160, val::16>>
+      %Instruction{command: :if_icmplt, arg: val} -> <<0x161, val::16>>
+      %Instruction{command: :if_icmple, arg: val} -> <<0x162, val::16>>
+      %Instruction{command: :if_icmpgt, arg: val} -> <<0x163, val::16>>
+      %Instruction{command: :if_icmpge, arg: val} -> <<0x164, val::16>>
+
+      %Instruction{command: :ior}  -> <<0x80>>
+      %Instruction{command: :iand} -> <<0x7E>>
+
+      %Instruction{command: :iload_0} -> <<0x1A>>
+      %Instruction{command: :iload_1} -> <<0x1B>>
+      %Instruction{command: :iload_2} -> <<0x1C>>
+      %Instruction{command: :iload_3} -> <<0x1D>>
+      %Instruction{command: :iload, arg: val} -> <<0x15, val>>
+
+      %Instruction{command: :ldc_w, arg: val} -> <<0x13, val::16>>
+
+      %Instruction{command: :invokestatic, arg: val} -> <<0xB8, val::16>>
+
+      %Instruction{command: :ireturn} -> <<0xAC>>
+
+      %Instruction{command: :goto, arg: val} -> <<0xA7, val::16>>
+    end
+  end
+
+  @spec bytify_method_access_flags(list()) :: binary()
+  defp bytify_method_access_flags(flags) do
+    mask = Enum.reduce(flags, 0, fn flag, acc ->
+      case flag do
+        :public    -> acc ||| 0x0001
+        :private   -> acc ||| 0x0002
+        :protected -> acc ||| 0x0004
+        :static    -> acc ||| 0x0008
+      end
+    end)
+
+    <<mask::16>>
+  end
+
+
+  @spec bytify_interfaces(list()) :: binary()
+  defp bytify_interfaces(_interfaces) do
+    <<>>
+  end
+
+  @spec bytify_fields(list()) :: binary()
+  defp bytify_fields(_fields) do
+    <<>>
+  end
+end
