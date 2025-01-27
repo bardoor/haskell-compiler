aa6853f3f58f52423c32ed8a233faf646de4fda2
bardoor <dmitrybartenev58@gmail.com>
2025-01-27 21:53:11 +0300

    feat(compiler): вернул вывод весёлых деревьев при компиляции

--- semantic/lib/compiler.ex
+++ semantic/lib/compiler.ex
@@ -3,34 +3,48 @@ defmodule Semantic do
   alias Generators.GenClass
   alias Semantic.Transformers
 
-  def start(str) do
-    #IO.puts("""
-    #     @@@           @@@           @@@
-    #   @@@@@@@       @@@@@@@       @@@@@@@
-    # @@@@@@@@@@@   @@@@@@@@@@@   @@@@@@@@@@@
-    #  @@@@@@@@       @@@@@@@       @@@@@@@
-    #     @@@           @@@           @@@
-    #      |             |             |
-    #      |             |             |
-    #      |             |             |
-    #    Haskell compiler. Enjoy the ride
-    #""")
+  def compile(code, output_dir) do
+    IO.puts("""
+         @@@           @@@           @@@
+       @@@@@@@       @@@@@@@       @@@@@@@
+     @@@@@@@@@@@   @@@@@@@@@@@   @@@@@@@@@@@
+      @@@@@@@@       @@@@@@@       @@@@@@@
+         @@@           @@@           @@@
+          |             |             |
+          |             |             |
+          |             |             |
+        Haskell compiler. Enjoy the ride
+    """)
 
-    {:ok, tree} = ParserBridge.parse(str)
+    {:ok, tree} = ParserBridge.parse(code)
 
     transformed = tree
     |> Transformers.link_funcs_and_types
     |> Transformers.index_locals
     |> IO.inspect()
 
-    class = GenClass.new()
+    GenClass.new()
     |> GenClass.generate(transformed)
-
+    |> save_bytecode(output_dir)
 
   end
 
   def put_error(message) do
     IO.puts(:stderr, "\e[31m" <> message <> "\e[0m")
+  end
+
+  defp save_bytecode(class, dir) do
+    File.mkdir_p!(dir)
+    path = Path.join(dir, "#{class.this_class}.class")
+
+    case File.open(path, [:write, :binary]) do
+      {:ok, file} ->
+        IO.binwrite(file, GenBytecode.generate(class))
+        File.close(file)
+
+      {:error, reason} ->
+        put_error(to_string(reason))
+    end
   end
 
 end
